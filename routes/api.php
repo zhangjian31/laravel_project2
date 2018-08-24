1c5d4f7e47cde74bd4006e069a3fe3ec202fefbd
zhangjian1 <zhangjian1@yixia.com>
2018-08-24 18:50:24 +0800

    注册路由

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use App\Article;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,3 +20,30 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::any('haha',function(){
     return 'any';
 });
+Route::get('articles', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return Article::all();
+});
+
+Route::get('articles/{id}', function($id) {
+    return Article::find($id);
+});
+
+Route::post('articles', function(Request $request) {
+    return Article::create($request->all);
+});
+
+Route::put('articles/{id}', function(Request $request, $id) {
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+
+    return $article;
+});
+
+Route::delete('articles/{id}', function($id) {
+    Article::find($id)->delete();
+
+    return 204;
+});
+
